607b2c0cb532fc34e997a4a629cdedce5b2916f7
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2017-03-29 14:48:43 -0300

    Initial Commit

---
+++ pList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  pList
+//
+//  Created by Eduardo Vital Alencar Cunha on 29/03/17.
+//  Copyright © 2017 Vital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
